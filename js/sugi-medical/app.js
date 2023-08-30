a32d3889ed1513f9ed7458acdd8b65a6eb3fcb6c
mayzinoo <mayzinoo@metateammyanmar.com>
2023-08-30 15:49:01 +0630

    mzo-no16-18-28-46-73-74-77-78-92-fixed

--- js/sugi-medical/app.js
+++ js/sugi-medical/app.js
@@ -111,3 +111,21 @@ function alignElementsBasedOnHeight() {
 // Call the alignment function when the page loads
 window.addEventListener("resize", alignElementsBasedOnHeight);
 window.addEventListener('load', alignElementsBasedOnHeight);
+
+// tabs
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
