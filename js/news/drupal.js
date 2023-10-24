dd506120351e0261f9d5087ddaec39fc48a5eb1e
takuto-murakami2 <takuto-murakami@sugi-pharmacy.co.jp>
2023-10-24 15:51:15 +0900

    ニュース一覧でPDFマークをつける_追加_ニュース

--- js/news/drupal.js
+++ js/news/drupal.js
@@ -66,11 +66,23 @@ async function updateArticleLists(category, searchKeyword) {
     console.log(data);
     if (ulElement) {
       ulElement.innerHTML = ''; // リストをクリア
-
       data.data.forEach((article, index) => {
         const liElement = document.createElement('li');
         liElement.className = 'news-list';
-        liElement.innerHTML = `
+        if (article.attributes.body === null) {
+          yliElement.innerHTML = `
+          <li class="news-list">
+            <a href="#" class="article-link" data-article-id="${article.id}">
+              <div class="newslist-header">
+                <span class="news-date">${article.attributes.field_date}</span>
+                <span class="news-info">${article.attributes.field_list}</span>
+              </div>
+              <p class="newslist-desc pdf" id="article-title${index + 1}">${article.attributes.title}</p>
+            </a>
+          </li>
+          `;
+        } else {
+          liElement.innerHTML = `
           <li class="news-list">
             <a href="#" class="article-link" data-article-id="${article.id}">
               <div class="newslist-header">
@@ -80,13 +92,14 @@ async function updateArticleLists(category, searchKeyword) {
               <p class="newslist-desc text-02" id="article-title${index + 1}">${article.attributes.title}</p>
             </a>
           </li>
-        `;
+          `;
+        }
         ulElement.appendChild(liElement);
       });
     }
   } catch (error) {
-    console.error('エラーが発生しました:', error);
-  }
+  console.error('エラーが発生しました:', error);
+}
 }
 
 // ページ番号を変更して記事を更新する関数
