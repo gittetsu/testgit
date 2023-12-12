e26472f302d9a44e4551e9fd6561784708fe597c
takuto-murakami2 <takuto-murakami@sugi-pharmacy.co.jp>
2023-12-12 15:02:27 +0900

    CORPORATESITE-71_1_0

--- js/load.js
+++ js/load.js
@@ -1,23 +1,21 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 	const includeHTML = (el, url) => {
-	  fetch(url)
-		.then((response) => {
-		  if (response.ok) {
-			return response.text();
-		  } else {
-			throw new Error(`Error ${response.status}: ${response.statusText}`);
-		  }
-		})
-		.then((html) => {
-		  el.outerHTML = html;
-		})
-		.catch((error) => {
-		  let message = error.message || "Error loading the file, verify that you are making the request by http or https";
-		  el.outerHTML = `<div><p>${message}</p></div>`;
+		const xhr = new XMLHttpRequest();
+		xhr.addEventListener("readystatechange", (e) => {
+			if (xhr.readyState !== 4) return;
+			if (xhr.status >= 200) {
+				el.outerHTML = xhr.responseText;
+			} else {
+				let message = xhr.statusText || "Error loading the file, verify that you are making the request by http or https";
+				el.outerHTML = `<div><p>Error ${xhr.status}: ${message}</p></div>`;
+			}
 		});
+
+		xhr.open("GET", url, false);
+		xhr.setRequestHeader("Content-type", "text/html; charset=utf-8");
+		xhr.send();
 	};
-  
 	document
-	  .querySelectorAll("[data-include]")
-	  .forEach((el) => includeHTML(el, el.getAttribute("data-include")));
-  });
+		.querySelectorAll("[data-include]")
+		.forEach((el) => includeHTML(el, el.getAttribute("data-include")));
+});
