305d434114e526a9142190a1e8a7e70370a7d6d0
takuto-murakami2 <takuto-murakami@sugi-pharmacy.co.jp>
2023-12-25 11:33:07 +0900

    CORPORATESITE-104

--- js/drupal.js
+++ js/drupal.js
@@ -3,13 +3,17 @@ const apiUrl = 'https://d37m9cibsc5611.cloudfront.net/jsonapi/node/sugi_hd';
 
 // カテゴリーごとに記事を取得して表示
 async function updateArticleLists(category) {
+  console.log("test1");
   try {
     // カテゴリーごとのフィルター条件を設定
     const filter = category === 'all' ? '' : `&filter[field_list]=${category}`;
+    const noticefilter = 'filter[ex1][condition][path]=field_notice&filter[ex1][condition][operator]=IN&filter[ex1][condition][value][1]=1&filter[ex1][condition][value][2]=3'
 
     // JSON APIからデータを取得
-    const response = await fetch(`${apiUrl}?sort=-field_date${filter}&page[limit]=5`);
+    const response = await fetch(`${apiUrl}?sort=-field_date${filter}&page[limit]=5&${noticefilter}`);
     const data = await response.json();
+    console.log(response);
+    console.log(data);
 
     // 取得したデータから記事リストを生成
     const ulElement = document.getElementById(category);
@@ -68,6 +72,52 @@ async function updateArticleLists(category) {
   }
 }
 
+// NOTICELIST用の関数
+async function updateNoticeList() {
+  try {
+    // JSON APIからNOTICELISTのデータを取得
+    const noticefilter2 = 'filter[ex1][condition][path]=field_notice&filter[ex1][condition][operator]=IN&filter[ex1][condition][value][1]=2&filter[ex1][condition][value][2]=3'
+    const response2 = await fetch(`${apiUrl}?sort=-field_date&${noticefilter2}`);
+    const data2 = await response2.json();
+    console.log("test2");
+    console.log(data2);
+  
+    // NOTICELIST用のリスト要素を取得
+    const noticeListElement = document.getElementById('noticelist');
+    // const noticeListElement = document.getElementById('li');
+    if (noticeListElement) {
+      noticeListElement.innerHTML = ''; // リストをクリア
+      data2.data.forEach((article, index) => {
+        // const liElement2 = document.createElement('li');
+        const liElement2 = document.createElement('noticelist');
+        let pdflink = "";
+        if (article.attributes.body === null) {
+          // 条件1: bodyがnullの場合、PDFへ遷移
+          if (article.attributes.field_pdf.value) {
+            pdflink = `/pdf/${article.attributes.field_pdf.value}" target="_blank`;
+          }
+        } else {
+          // 条件2: bodyがnullでない場合、記事へ遷移
+          pdflink = `/sugi-nursingcare/news/article.html?id=${article.id}`;
+        }    
+        // liElement2.className = 'notice-list';
+        liElement2.className = 'notice-list-item';
+        liElement2.innerHTML = `
+                <li class="notice-item">
+                <a href="#">
+                <span class="notice-date">${article.attributes.field_date}</span>
+                <p class="notice-desc id="article-title${index + 1}">${article.attributes.title}</p>
+                </a>
+                </li>
+        `;
+        noticeListElement.appendChild(liElement2);
+      });
+    }
+  } catch (error) {
+    console.error('エラーが発生しました:', error);
+  }
+}
+
 // タブリンクにクリックイベントを追加
 const tabLinks = document.querySelectorAll('.tablinks');
 tabLinks.forEach(link => {
@@ -80,4 +130,5 @@ tabLinks.forEach(link => {
 // ページ読み込み時にデフォルトのカテゴリーで記事を表示
 window.addEventListener('load', () => {
   updateArticleLists('all'); // デフォルトは 'all' カテゴリー
+  updateNoticeList(); // NOTICELISTの記事を表示
 });
