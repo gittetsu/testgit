0ee0f6e5d17dd84e5375f8554174030f68ca467d
takuto-murakami2 <takuto-murakami@sugi-pharmacy.co.jp>
2024-05-17 16:03:54 +0900

    HDWEB-86_sugihd

--- js/ir/drupal.js
+++ js/ir/drupal.js
@@ -1,6 +1,7 @@
 // JSON APIのエンドポイントURLを指定
 const apiUrl = 'https://d1vyjchtv8ee5.cloudfront.net/jsonapi/node/sugi_hd?sort=-field_date&filter[field_list]=IR情報&page[limit]=5';
 const fileApiUrl = 'https://d1vyjchtv8ee5.cloudfront.net/jsonapi/file/file';
+const announcementUrl = 'https://d1vyjchtv8ee5.cloudfront.net/jsonapi/node/ir_announcement/837780a3-a0a0-42cd-9b9c-479493e51d2b';
 
 // 日付フォーマット変更
 function formatDate(dateString) {
@@ -82,5 +83,21 @@ async function updateArticleTitles() {
     }
 }
 
+async function updateAnnouncement() {
+    try {
+        // JSON APIからデータを取得
+        const announcementresponse = await fetch(announcementUrl);
+        const announcementdata = await announcementresponse.json();
+        const announcementdate = announcementdata.data.attributes.field_ir_announcement_date;
+        const announcementbody = announcementdata.data.attributes.field_ir_announcement_body;
+
+        document.getElementById('announcement_date').innerHTML = announcementdate;
+        document.getElementById('announcement_body').innerHTML = announcementbody;
+    } catch (error) {
+        console.error('エラーが発生しました:', error);
+    }
+}
+
 // ページ読み込み時に記事タイトルを更新
 window.addEventListener('load', updateArticleTitles);
+window.addEventListener('load', updateAnnouncement);
