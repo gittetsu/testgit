d1a2a408a738694749021f704869682e2f58a107
k-yamada <k-yamada@insity.jp>
2024-09-26 16:03:37 +0900

    CORPMEDIE-17 footerからのアンカースクロールを正しく動かす

--- js/common.js
+++ js/common.js
@@ -286,6 +286,9 @@ function commonload() {
                 offset = 0;
             }
         }
+    if (document.querySelector("header").classList.contains("sugi-medical-header")) {
+      offset = 85;
+    }
         const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - offset; // Include current
         const startPosition = window.pageYOffset;
         const distance = targetPosition - startPosition;
