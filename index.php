744ed8c3eb681c4bfdb98085cb6a6004d7113c16
shibasaki <t.shibasaki@wac.co.jp>
2025-02-20 12:33:45 +0900

    メッセージ’ git commit -m メッセージ’

---
+++ index.php
@@ -0,0 +1,311 @@
+<?php
+/**
+ * The index for our theme
+ *
+ * @link https://webdesign.wac.co.jp/
+ *
+ * @package WordPress
+ * @subpackage WAC WAB DESIGN HP
+ * @author WORK ASSIST
+ * @since 2025. 1. 1
+ * @version 1.0.0
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="">
+
+  <article class="article_1">
+    <div id="hwrapper">
+
+      <div class="swiper">
+        <div class="swiper-wrapper">
+
+          <div class="swiper-slide">
+            <img class="" src="" alt="" data-swiper-parallax-x="100%" />
+          </div>
+
+          <div class="swiper-slide">
+            <img class="pc" src="<?php echo get_template_directory_uri(); ?>/images/top_img_001.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="tab" src="<?php echo get_template_directory_uri(); ?>/images/top_img_011.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="sp-x" src="<?php echo get_template_directory_uri(); ?>/images/top_img_011.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="sp-y" src="<?php echo get_template_directory_uri(); ?>/images/top_img_014.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+          </div>
+
+          <div class="swiper-slide">
+            <img class="pc" src="<?php echo get_template_directory_uri(); ?>/images/top_img_010.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="tab" src="<?php echo get_template_directory_uri(); ?>/images/top_img_012.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="sp-x" src="<?php echo get_template_directory_uri(); ?>/images/top_img_012.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="sp-y" src="<?php echo get_template_directory_uri(); ?>/images/top_img_015.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+          </div>
+
+          <div class="swiper-slide">
+            <img class="pc" src="<?php echo get_template_directory_uri(); ?>/images/top_img_003.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="tab" src="<?php echo get_template_directory_uri(); ?>/images/top_img_013.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="sp-x" src="<?php echo get_template_directory_uri(); ?>/images/top_img_013.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+            <img class="sp-y" src="<?php echo get_template_directory_uri(); ?>/images/top_img_016.jpg" alt=""
+              data-swiper-parallax-x="100%" />
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </article>
+
+  <article class="article_2 show_scale">
+    
+    <div class="about_top">
+      <h2><span class="gradient_title">01 ABOUT US</span></h2>
+      <p class="summary_top">
+        私たち株式会社ワークアシストは、ウェブサイトの制作、運用、保守と関連する業務のサポートを行う企業です。良質なウェブサイトは、サイトを利用する方にも運営される方にも価値ある体験を提供するものと考え、より良いウェブサイトづくりを目指し努力しております。
+      </p>
+      <div class="link-layout">
+        <a href="<?php echo home_url('/about/'); ?>" class="link">会社情報を見る ></a>
+      </div>
+    </div>
+
+    <div class="slide_text_wrap">
+      <ul class="slide_text_4">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_2">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_1">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_4">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_1">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_2">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_3">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_2">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_1">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_4">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_1">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_2">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+      <ul class="slide_text_4">
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+        <li>WAC WEB DESIGN</li>
+      </ul>
+    </div>
+  </article>
+
+  <article class="article_3">
+
+    <section class="section_top">
+
+      <div class="obj_1">
+        <img class="" src="<?php echo get_template_directory_uri(); ?>/images/top_cont_img_002.png" alt="" />
+      </div>
+
+      <div class="thumbnail section_text_r">
+        <div class="contents-summary_top obj_4">
+          <h2><span class="gradient_title">02 TRUST</span></h2>
+          <p class="summary_top glad">
+            私たちは、創業から30年以上にわたり、真摯に、真面目に、そして誠実に仕事と向き合い、お客様との信頼を築いてまいりました。今後、社会がどのように変化しようともその姿勢が変わることはありません。
+          </p>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section_top">
+
+      <div class="obj_2">
+        <img class="" src="<?php echo get_template_directory_uri(); ?>/images/top_cont_img_001.png" alt="" />
+      </div>
+
+      <div class="thumbnail section_text_l">
+        <div class="contents-summary_top obj_5">
+          <h2><span class="gradient_title">03 EXPERIENCE</span></h2>
+          <p class="summary_top">
+            私たちは、金融・製造・通信・流通・公共分野など、多種多様な業種のお客様のご要望に応え続けてきたことで、技術、知識、経験を積み上げることできました。
+          </p>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section_top">
+
+      <div class="obj_3">
+        <img class="" src="<?php echo get_template_directory_uri(); ?>/images/top_cont_img_003.png" alt="" />
+      </div>
+
+      <div class="thumbnail section_text_ll">
+        <div class="contents-summary_top obj_6">
+          <h2><span class="gradient_title">04 SUPPORT</span></h2>
+          <p class="summary_top">
+            私たちは、前例や形式にとらわれず、個々のお客様に寄り添い、課題解決と目標達成に向けて価値あるサービスの提供を続けてまいります。
+          </p>
+        </div>
+      </div>
+
+      <div class="thumbnail section_text_rr">
+        <div class="contents-summary_top obj_7">
+          <h2><span class="gradient_title">05 TECHNOLOGY</span></h2>
+          <p class="summary_top">
+            私たちは、創業よりエンジニアの人材教育に力を入れて取り組んでまいりました。在籍する高い技術力と豊富な経験を持つエンジニアが、お客様のビジネスをサポートいたします。
+          </p>
+        </div>
+      </div>
+
+      <div class="btn-layout">
+        <a href="<?php echo esc_url( home_url( '/service-price/' ) ); ?>" class="btn">サービス一覧を見る ></a>
+      </div>
+
+    </section>
+
+    <section class="top-news">
+
+      <div class="contents-title fadein">
+        <p>NEWS</p>
+      </div>
+
+      <?php $args = [
+        'post_type' => 'post',
+        'category_name' => 'news',
+        'posts_per_page' => 3
+      ]; ?>
+
+      <?php $the_query = new WP_Query($args); ?>
+
+      <?php if ($the_query->have_posts()): ?>
+
+        <?php while ($the_query->have_posts()):
+          $the_query->the_post(); ?>
+
+          <?php $post_data = get_posts($args); ?>
+          <?php $post_id = $post_data->ID; ?>
+
+          <section class="post-item fadein">
+
+            <div class="notice">
+
+              <div class="article-time">
+                <a href="<?php echo esc_url(get_permalink($post_data->ID, '')); ?>"
+                  class="p-transition"><?php the_time(get_option('date_format')) ?></a>
+              </div>
+
+              <div class="article-title">
+                <?php the_title('<p><a href="' . esc_url(get_permalink($post_data->ID, '')) . '">', '</a></p>'); ?>
+              </div>
+
+            </div>
+
+          </section>
+
+        <?php endwhile; ?>
+
+        <?php wp_reset_query(); ?>
+
+      <?php else: ?>
+        <div class="error">
+          <p>お探しの記事は見つかりませんでした。</p>
+        </div>
+      <?php endif; ?>
+
+      <div class="link-layout">
+        <a href="<?php echo home_url('/category/news/'); ?>" class="link">ニュース一覧を見る ></a>
+      </div>
+
+    </section>
+
+  </article>
+
+  <?php get_footer(); ?>
